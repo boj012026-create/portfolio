27fdbf5976daf1b5291e699a9202257d3fa9639b
boj012026-create <boj012026@gmail.com>
2026-08-24 14:48:10 +0200

    chore(repoCard): give link url a type

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import './App.css'
 
 function App() {
 
+  const sBoardURL: string = "https://boj012026-create.github.io/DaftPunkSoundboard/";
+
   return (
     <>
     <h2 className="text-4xl">Repo</h2>
-    <RepoCard img={soundBoardthumb} link="https://boj012026-create.github.io/DaftPunkSoundboard/"/>
+    <RepoCard img={soundBoardthumb} link={sBoardURL}/>
     </>
   )
 }
