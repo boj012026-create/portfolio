7d3dfd28af1254194c9c1e2026c4b81d94db0550
boj012026-create <boj012026@gmail.com>
2026-08-31 09:16:31 +0200

    chorei(app): remove prototype link and path prints

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import soundBoardthumb from './assets/thumbnails/soundboard.jpg'
 import RepoCard from './components/RepoCard'
 import './App.css'
 import repoJson from './assets/json/repoStats.json'
 import thumbImg from './services/thumbImg.ts'
 
 function App() {
-  const sBoardURL: string = "https://boj012026-create.github.io/DaftPunkSoundboard/";
   console.log("thumb in app", thumbImg)
 
   const ob: Record<string, string> = {
@@ -17,10 +15,8 @@ function App() {
   return (
     <>
     <h2 className="text-4xl">Repo</h2>
-    <RepoCard img={soundBoardthumb} link={sBoardURL}/>
     { repoJson.map( repo => (
       <>
-      <p>repo name from JSON import{repo.name}</p>
      <RepoCard id={repo.id} img={thumbImg[`${repo.name}.jpg`]} link={repo.homepage}/>
      
      </>
